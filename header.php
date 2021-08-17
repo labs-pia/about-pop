64a42be811c64d4c075eb9270d52d3688fbe60ae
labs-pia <89085683+labs-pia@users.noreply.github.com>
2021-08-17 17:52:06 +0200

    Update header.php

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
 				the_custom_logo();
 				if ( is_front_page() && is_home() ) :
 					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					
 					<?php
 				endif;
 				$about_pop_description = get_bloginfo( 'description', 'display' );
