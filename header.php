547357d466fb8bc39b25ebdb5e13e0d709dfaeaa
labs-pia <89085683+labs-pia@users.noreply.github.com>
2021-08-17 17:56:11 +0200

    Update header.php

--- header.php
+++ header.php
@@ -26,10 +26,10 @@
 		<div class="logo-wrapper">
 			<div class="site-branding">
 				<?php
-				the_custom_logo();
+				
 				if ( is_front_page() && is_home() ) :
 					?>
-					
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<?php
 				endif;
 				$about_pop_description = get_bloginfo( 'description', 'display' );
