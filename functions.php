d19e25351d89b5d057bc7d1235fff798e33b93c0
labs-lars <lars@labs.project2010.org>
2021-08-18 16:41:54 +0200

    fix

--- functions.php
+++ functions.php
@@ -197,72 +197,8 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-function pop_buero_allowedtags() {
-	// Add custom tags to this string
-	return '<script>,<style>,<br>,<em>,<i>,<ul>,<ol>,<li>,<a>,<p>,<img>,<video>,<audio>'; 
-}
-
-if ( ! function_exists( 'pop_buero_custom_wp_trim_excerpt' ) ) : 
-
-    function pop_buero_custom_wp_trim_excerpt($excerpt) {
-    global $post;
-    $raw_excerpt = $excerptt;
-        if ( '' == $excerpt ) {
-
-            $excerpt = get_the_content('');
-            $excerpt = strip_shortcodes( $excerpt );
-            $excerpt = apply_filters('the_content', $excerpt);
-            $excerpt = str_replace(']]>', ']]&gt;', $excerpt);
-            $excerpt = strip_tags($excerpt, pop_buero_allowedtags()); /*IF you need to allow just certain tags. Delete if all tags are allowed */
-
-            //Set the excerpt word count and only break after sentence is complete.
-                $excerpt_word_count = 75;
-                $excerpt_length = apply_filters('excerpt_length', $excerpt_word_count); 
-                $tokens = array();
-                $excerptOutput = '';
-                $count = 0;
-
-                // Divide the string into tokens; HTML tags, or words, followed by any whitespace
-                preg_match_all('/(<[^>]+>|[^<>\s]+)\s*/u', $excerpt, $tokens);
-
-                foreach ($tokens[0] as $token) { 
-
-                    if ($count >= $excerpt_word_count && preg_match('/[\,\;\?\.\!]\s*$/uS', $token)) { 
-                    // Limit reached, continue until , ; ? . or ! occur at the end
-                        $excerptOutput .= trim($token);
-                        break;
-                    }
-
-                    // Add words to complete sentence
-                    $count++;
-
-                    // Append what's left of the token
-                    $excerptOutput .= $token;
-                }
-
-            $excerpt = trim(force_balance_tags($excerptOutput));
-
-                $excerpt_end = ' <a href="'. esc_url( get_permalink() ) . '">' . '&nbsp;&raquo;&nbsp;' . sprintf(__( 'Read more about: %s &nbsp;&raquo;', 'wpse' ), get_the_title()) . '</a>'; 
-                $excerpt_more = apply_filters('excerpt_more', ' ' . $excerpt_end); 
-
-                //$pos = strrpos($wpse_excerpt, '</');
-                //if ($pos !== false)
-                // Inside last HTML tag
-                //$wpse_excerpt = substr_replace($wpse_excerpt, $excerpt_end, $pos, 0); /* Add read more next to last word */
-                //else
-                // After the content
-                $excerpt .= $excerpt_end; /*Add read more in new paragraph */
-
-            return $excerpt;   
-
-        }
-        return apply_filters('pop_buero_custom_wp_trim_excerpt', $excerpt, $raw_excerpt);
-    }
-
-endif; 
-
 remove_filter('get_the_excerpt', 'wp_trim_excerpt');
-add_filter('get_the_excerpt', 'pop_buero_custom_wp_trim_excerpt'); 
+//add_filter('get_the_excerpt', 'pop_buero_custom_wp_trim_excerpt'); 
 
 
 /**
