4c03b2b4896b2199786e5ddc8131a8d4cc95da40
labs-lars <lars@labs.project2010.org>
2021-08-18 14:39:35 +0200

    added about_pop_excerpt() function

--- functions.php
+++ functions.php
@@ -171,6 +171,33 @@ function about_pop_scripts() {
 add_action( 'wp_enqueue_scripts', 'about_pop_scripts' );
 
 /**
+ * Special excerpt function
+ * cuts a string by $maxchar
+ */
+function about_pop_excerpt($text, $maxchar, $end='...') {
+    if (strlen($text) > $maxchar || $text == '') {
+        $words = preg_split('/\s/', $text);      
+        $output = '';
+        $i      = 0;
+        while (1) {
+            $length = strlen($output)+strlen($words[$i]);
+            if ($length > $maxchar) {
+                break;
+            } 
+            else {
+                $output .= " " . $words[$i];
+                ++$i;
+            }
+        }
+        $output .= $end;
+    } 
+    else {
+        $output = $text;
+    }
+    return $output;
+}
+
+/**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
