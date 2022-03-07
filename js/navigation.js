9f9cae1134841f777c5fc9fcc0d02a33a94a62c9
labs-pia <pia@labs.project2010.org>
2022-03-07 16:09:23 +0100

    test navigation

--- js/navigation.js
+++ js/navigation.js
@@ -6,44 +6,35 @@ var schalter = false;
 button.onclick = function () {
 	if (schalter == false) {
 		open();
-		schalter = true;
 	} else if (schalter == true) {
 		close();
     }
 };
 
 function open() {
-  siteNavigation.animate({
-    "height":"100vh"
-  }, 300);
+  siteNavigation.classList.remove='closeanimation';
+  siteNavigation.className='openanimation';
   setTimeout("siteNavigation.style.height = '100vh';", 280);
-	
-  menu.animate({
-    "display":"flex"
-  }, 300);
+  setTimeout("menu.style.height = '100vh';", 280);
   setTimeout("menu.style.display = 'flex';", 150);
+  menu.style.display = 'flex';
   menu.style.width = "100vw";
   menu.style.margin = "auto";
   menu.style.padding = "0";
-  menu.style.height = "70vh";
+  menu.style.height = "100vh";
   menu.style.flexDirection = "column";
-
   schalter = true;
-}
+};
 
 function close() {
-  siteNavigation.animate({
-    "height":"100px"
-  }, 300);
+  siteNavigation.classList.remove='openanimation';
+  siteNavigation.className='closeanimation';
   setTimeout("siteNavigation.style.height = '100px';", 280);
-	
-  menu.animate({
-    "display":"none"
-  }, 300);
+  setTimeout("menu.style.height = '0';", 280);
   setTimeout("menu.style.display = 'none';", 100);
-
   schalter = false;
 }
+
 
 // Smooth scoller
 // by clicking on a menu item, the function handles the href attribute and scrolls to it
