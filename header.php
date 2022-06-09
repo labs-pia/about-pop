6c81a74b58f706c2264a8214a18c880c84f8c6bf
labs-pia <pia@labs.project2010.org>
2022-06-09 13:51:11 +0200

    add cache control

--- header.php
+++ header.php
@@ -11,11 +11,9 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!--
 	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
 	<meta http-equiv="Pragma" content="no-cache" />
 	<meta http-equiv="Expires" content="0" />
-	-->
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
