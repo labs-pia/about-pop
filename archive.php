0589621752823b6d8a5db1a6424f4707b95f00d6
labs-pia <pia@labs.project2010.org>
2022-06-14 18:50:07 +0200

    add listing for archive entries

--- archive.php
+++ archive.php
@@ -21,64 +21,68 @@ get_header();
 				?>
 			</header><!-- .page-header -->
 			
-			<div id="frame"> 
-				<?php // Start the Loop
-				while ( have_posts() ) :
-					the_post();
-					?>
-					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<div class="event_thumbnail">
-							<?php about_pop_post_thumbnail(); ?>
-						</div>
-						<div class="post-content">
-							<?php
-							if ( is_singular() ) :
-								the_title( '<h2 class="entry-title">', '</h2>' );
-							else :
-								the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-							endif;
+			<div id="frame">
+				<ul class="wp-block-latest-posts__list is-grid columns-5 aligncenter programm wp-block-latest-posts">
+					<?php // Start the Loop
+					while ( have_posts() ) :
+						echo '<li>';
+						the_post();
+						?>
+							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+								<div class="event_thumbnail">
+									<?php about_pop_post_thumbnail(); ?>
+								</div>
+								<div class="post-content">
+									<?php
+									if ( is_singular() ) :
+										the_title( '<h2 class="entry-title">', '</h2>' );
+									else :
+										the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+									endif;
 
-							if ( 'post' === get_post_type() ) :
-							endif; ?>
-							
-							<?php
-							the_content(
-								sprintf(
-									wp_kses(
-										/* translators: %s: Name of current post. Only visible to screen readers */
-										__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'about-pop' ),
-										array(
-											'span' => array(
-												'class' => array(),
+									if ( 'post' === get_post_type() ) :
+									endif; ?>
+									
+									<?php
+									the_content(
+										sprintf(
+											wp_kses(
+												/* translators: %s: Name of current post. Only visible to screen readers */
+												__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'about-pop' ),
+												array(
+													'span' => array(
+														'class' => array(),
+													),
+												)
 											),
+											wp_kses_post( get_the_title() )
 										)
-									),
-									wp_kses_post( get_the_title() )
-								)
-							);
+									);
 
-							wp_link_pages(
-								array(
-									'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'about-pop' ),
-									'after'  => '</div>',
-								)
-							);
-							?>
-						</div><!-- .entry-content -->
+									wp_link_pages(
+										array(
+											'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'about-pop' ),
+											'after'  => '</div>',
+										)
+									);
+									?>
+								</div><!-- .entry-content -->
 
-					</article>
-				<?php 
+							</article>
+						</li>
+					<?php 
 
-				endwhile;
+					endwhile;
 
-				the_posts_navigation();
+					the_posts_navigation();
 
-			else :
+		else :
 
-				get_template_part( 'template-parts/content', 'none' );
+			get_template_part( 'template-parts/content', 'none' );
 
-			endif;
-			?>
+		endif;
+		?>
+			</ul>
 		</div>
 	</main><!-- #main -->
 
